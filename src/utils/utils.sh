5d5b4083de9884711d1f53d001b9c5ce415c4c7f
lizzha <zhangli8810@gmail.com>
2017-03-10 07:47:33 +0000

    Update indent in util.sh

--- src/utils/utils.sh
+++ src/utils/utils.sh
@@ -40,21 +40,21 @@ wait_until_process_stops()
 }
 
 pid_guard() {
-  echo "------------ STARTING `basename $0` at `date` --------------" | tee /dev/stderr
-  pidfile=$1
-  name=$2
+    echo "------------ STARTING `basename $0` at `date` --------------" | tee /dev/stderr
+    pidfile=$1
+    name=$2
 
-  if [ -f "$pidfile" ]; then
-    pid=$(head -1 "$pidfile")
+    if [ -f "$pidfile" ]; then
+        pid=$(head -1 "$pidfile")
 
-    if [ -n "$pid" ] && [ -e /proc/$pid ]; then
-      echo "$name is already running, please stop it first"
-      exit 1
+        if [ -n "$pid" ] && [ -e /proc/$pid ]; then
+            echo "$name is already running, please stop it first"
+            exit 1
+        fi
+
+        echo "Removing stale pidfile..."
+        rm $pidfile
     fi
-
-    echo "Removing stale pidfile..."
-    rm $pidfile
-  fi
 }
 
 find_systemd_dir()
